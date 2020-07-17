f069859f9e567c948bd7feca2c7ba4a9c0b31a13
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-07-17 16:09:58 +0300

    Full landing page with lorem text

---
+++ client/src/components/Landing/action-form.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ActionForm() {
+  return (
+    <>
+      <h2 className="text-center text-success">Регистрация</h2>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center">
+            Не откладывайте на потом ваше здоровье
+          </h1>
+          <p className="lead text-center">
+            Найчните действовать сегодня, и совсем скоро вы почуствуете
+            изменения в лучшую сторону!
+          </p>
+          <Link
+            class="btn btn-success btn-lg mx-auto"
+            to="/signup"
+            role="button"
+          >
+            Зарегистрироваться
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ActionForm;
