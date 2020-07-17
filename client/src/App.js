9c935be15782f7b8d4725221ccc58fe628d20d2f
Sergey <stype@yandex.ru>
2020-07-17 13:38:33 +0300

    Release 0

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,14 @@ import QuestMain from './components/questionnaire/QuestMain';
 import Program from './pages/program';
 import PrivateRoute from './components/auth/PrivateRoute';
 import Landing from './pages/landing';
+import Stepper from './components/Paginator';
 import sessionChecker from './components/hooks/auth-hook';
 
 export default function App() {
   // Проверяем наличие на сервере сессии для данного пользователя
   // !!! наличие сессии не гарантирует статус авторизированного пользователя
   // для авторизации необходимо получать с сервера доп. инф-ю о правах пользователя
-  // sessionChecker();
+  sessionChecker();
 
   return (
     <>
@@ -39,6 +40,9 @@ export default function App() {
           </Route>
           <Route path="/program">
             <Program />
+          </Route>
+          <Route path="/stepper">
+            <Stepper />
           </Route>
           <Route path="/">
             <Landing />
