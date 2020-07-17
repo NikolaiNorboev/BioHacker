158b910015a437b5b5fbb7dad997e69d9ceadb5d
Nikolai N <gippich@gmail.com>
2020-07-17 11:03:12 +0300

    question with logic in complite

--- client/src/components/questionnaire/PageOfQuest/index.js
+++ client/src/components/questionnaire/PageOfQuest/index.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import { getAnswer } from '../../../redux/actions/questionnaire';
 import './PageOfQuest.scss'
 
 function PageOfQuest(props) {
 
-  const { getNext, questionIndex, questionText, data, questionsCount} = props;
+  const { getNext, questionIndex, questionText, data, questionsCount, image, type, getPrev} = props;
+  const quest = useSelector(state => state.quest);
+  const dispatch = useDispatch();
+
+  let flag = false;
+  if (questionIndex > 1) flag = true;
+
+  function getAns(key) {
+    // quest = quest.map((third) => {
+    //   return (
+    //     third.questionIndex === questionIndex ?{
+    //       data.map(four => {
+    //         return (
+    //           if (four.key === key) {
+    //           four.questionOption = !four.questionOption;
+    //           }
+    //         )
+    //       
+    //     }
+    //   )
+    // })
+  }
 
   return(
     <div className="question">
      
       <p className="questionCount">{questionIndex}/{questionsCount}</p>
       <h3>{questionText}</h3>
+      <img className="question__img" src={image} alt="alt" />
       <ul className="questionUl">
         {data.length && data.map(second => {
           return(
             <li key={questionIndex + data.key} className="questionLi">
               <span className="questionSpan">{second.questionOption}</span>
-              <input className="questionInput"type="checkbox" defaultChecked={second.questionAnswer} />
+              <input className="questionInput"type={type} defaultChecked={second.questionAnswer} onClick={getAns(data.key)}/>
             </li>
           )
         })}
+      {flag && <button onClick={getPrev}>Previos</button>}
       <button onClick={getNext}>Next</button>
       </ul>
       
