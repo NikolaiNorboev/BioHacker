cb88c4d5934a16578d0ec30e470cb785c95e9ffc
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-07-19 17:43:06 +0300

    Add push subscription route

--- server/app.js
+++ server/app.js
@@ -4,20 +4,19 @@ import errorHandlers from './middleware/error-handlers.js';
 import indexRouter from './routes/index.js';
 import authRouter from './routes/auth.js';
 import courseRouter from './routes/course.js';
+import serviceWorkerRoute from './routes/serviceWorker.js';
 
 const app = express();
 
 useMiddleware(app);
 
-
 // Подключаем импортированные маршруты с определенным url префиксом.
-app.use("/api", indexRouter);
-app.use("/auth", authRouter);
-app.use("/course", courseRouter);
-
+app.use('/api', indexRouter);
+app.use('/auth', authRouter);
+app.use('/course', courseRouter);
+app.use(serviceWorkerRoute);
 
 // Обработка несуществующих запросов
 errorHandlers(app);
-
 
 export default app;
