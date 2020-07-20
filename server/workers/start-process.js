6260ea27a53bc9f97f2b2b17e79fd21c5823ac6e
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-07-20 15:17:40 +0300

    Add logic to start communication process

---
+++ server/workers/start-process.js
@@ -0,0 +1,16 @@
+import zeebe from 'zeebe-node';
+
+// Start new process instance
+async function startProcess(name, program, pushKey, auth, endpoint) {
+  const zbc = new zeebe.ZBClient('localhost:26500');
+  const result = await zbc.createWorkflowInstance('Process_1jbwj1x', {
+    name,
+    program,
+    pushKey,
+    auth,
+    endpoint,
+  });
+  return result;
+}
+
+export default startProcess;
