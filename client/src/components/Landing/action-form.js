12fa5f5ec3a3cb32aa94c2bbeff866657a1c595b
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-07-20 09:27:18 +0300

    Add some content to landing

--- client/src/components/Landing/action-form.js
+++ client/src/components/Landing/action-form.js
@@ -8,11 +8,10 @@ function ActionForm() {
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="display-4 text-center">
-            Не откладывайте на потом ваше здоровье
+            Не откладывайте на потом свое здоровье.
           </h1>
           <p className="lead text-center">
-            Найчните действовать сегодня, и совсем скоро вы почуствуете
-            изменения в лучшую сторону!
+            Начни действовать сейчас! Нужен всего один шаг.
           </p>
           <Link
             class="btn btn-success mx-auto"
--- client/src/components/Landing/reviews.js
+++ client/src/components/Landing/reviews.js
@@ -13,10 +13,13 @@ function Reviews() {
           />
           <div class="media-body">
             <h5 class="mt-0 mb-1">Максим Малышев</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-            vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-            vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Прошел программу Антистресс от Biohaker. Ребята, огромное вам
+            спасибо! У ребят отличный контент, грамотно структурированная
+            полезная информация. Много практики, антистресс методик. Нашел для
+            себя действенные, которые использую каждый день! Помимо этого
+            разобрался со своим питанием, определил продукты, негативно влияющие
+            на меня. Как результат – стал меньше нервничать, больше отдыхать,
+            появилась энергия на свой стартап.
           </div>
         </li>
         <li class="media my-4">
@@ -26,11 +29,13 @@ function Reviews() {
             alt="..."
           />
           <div class="media-body">
-            <h5 class="mt-0 mb-1">Светлана Чернова</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-            vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-            vulputate fringilla. Donec lacinia congue felis in faucibus.
+            <h5 class="mt-0 mb-1">Светлана Чернова</h5>Я поняла, что апатия и
+            упадок сил – это не норма, равно как и постоянная тяга к сладкому!
+            Проходили Детокс от Biohaker всей семьей для моральной поддержки.
+            Хотя поддержки от команды Biohaker было много, и программа так
+            построена, что сорваться не сможешь. Хочу пройти «Витаминизацию» и
+            «Антистресс». Но уже сейчас понимаю, что болеть вся семья стала
+            реже, у меня вообще нет тяги к сладкому, кожа выровнялась.
           </div>
         </li>
         <li class="media">
@@ -41,10 +46,12 @@ function Reviews() {
           />
           <div class="media-body">
             <h5 class="mt-0 mb-1">Анастасия Викторовна</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-            vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-            vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Раньше пила БАДы и никаких улучшений не было. Пошла на курс
+            Витаминизация, чтобы понять, что не так делаю. Энергия была на нуле,
+            голова постоянно болела, раздражалась по пустякам. Благодаря курсу
+            поняла, что у меня анемия и дефицит витаминов B9 и B12. Сейчас
+            восполняю свои дефициты. Cпасибо огромное за курс! Я чувствую себя
+            человеком, стала уделять внимание уходу за собой и своим телом.
           </div>
         </li>
       </ul>
