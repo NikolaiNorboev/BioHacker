0bc7ee2798a2d268bd22747cf079caa7056ab0cc
Sergey <stype@yandex.ru>
2020-07-20 17:30:01 +0300

    sessionChecker

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import Nav from './components/Navbar';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import Logout from './components/auth/Logout';
-import Info from './components/Info';
 import Program from './pages/program';
-import PrivateRoute from './components/auth/PrivateRoute';
+import SessionRoute from './components/auth/SessionRoute';
 import Landing from './pages/landing';
 import Stepper from './components/Stepper';
-import sessionChecker from './components/hooks/auth-hook';
+// import useSessionChecker from './components/hooks/auth-hook';
 import Thankyou from './components/program/Thankyou';
 import User from './pages/user';
 import ChannelSelection from './components/ChannelSelection';
@@ -17,46 +16,40 @@ import ChannelSelection from './components/ChannelSelection';
 
 
 export default function App() {
-  // Проверяем наличие на сервере сессии для данного пользователя
-  // !!! наличие сессии не гарантирует статус авторизированного пользователя
-  // для авторизации необходимо получать с сервера доп. инф-ю о правах пользователя
-  sessionChecker();
+  // useSessionChecker();
 
   return (
     <>
       <Router>
         <Nav />
         <Switch>
-          <Route path="/signup">
+          <SessionRoute path="/signup">
             <Signup />
-          </Route>
-          <Route path="/login">
+          </SessionRoute>
+          <SessionRoute path="/login">
             <Login />
-          </Route>
-          <Route path="/logout">
+          </SessionRoute>
+          <SessionRoute path="/logout">
             <Logout />
-          </Route>
-          <PrivateRoute path="/info">
-            <Info />
-          </PrivateRoute>
+          </SessionRoute>
           <Route path="/program">
             <Program />
           </Route>
-          <Route path="/thankYou">
+          <SessionRoute path="/thankYou">
             <Thankyou />
-          </Route>
-          <Route path="/stepper">
+          </SessionRoute>
+          <SessionRoute path="/stepper">
             <Stepper />
-          </Route>
-          <Route path="/user">
+          </SessionRoute>
+          <SessionRoute path="/user">
             <User />
-          </Route>
-          <Route path="/channel">
+          </SessionRoute>
+          <SessionRoute path="/channel">
             <ChannelSelection />
-          </Route>
-          <Route path="/">
+          </SessionRoute>
+          <SessionRoute exact path="/">
             <Landing />
-          </Route>
+          </SessionRoute>
         </Switch>
       </Router>
     </>
