b6bf0b931a90b7ccaa6693292850e12c068ebe58
Alexandr Kuznetsov <kuznecov.alexandr@gmail.com>
2020-07-20 10:04:05 +0300

    Center last batton on landing page

--- client/src/components/Landing/action-form.js
+++ client/src/components/Landing/action-form.js
@@ -5,21 +5,19 @@ function ActionForm() {
   return (
     <>
       <h2 className="text-center text-success">Регистрация</h2>
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container">
+      <div className="jumbotron jumbotron-fluid ">
+        <div className="container ">
           <h1 className="display-4 text-center">
             Не откладывайте на потом свое здоровье.
           </h1>
           <p className="lead text-center">
             Начни действовать сейчас! Нужен всего один шаг.
           </p>
-          <Link
-            class="btn btn-success mx-auto"
-            to="/signup"
-            role="button"
-          >
-            Зарегистрироваться
-          </Link>
+          <div className="mx-auto mt-5" style={{ width: '200px' }}>
+            <Link class="btn btn-success" to="/signup" role="button">
+              Зарегистрироваться
+            </Link>
+          </div>
         </div>
       </div>
     </>
